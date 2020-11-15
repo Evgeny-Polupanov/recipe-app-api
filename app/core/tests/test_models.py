c78bb3e0f4c3855c131d8ad698bdd6036b5471d8
Evgeny <polupanov.e@btc-s.ru>
2020-11-15 17:47:04 +0300

    configured postgres db

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,10 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = 'test@londonappdev.com'
         password = 'Testpass123'
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -27,7 +30,10 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser('test@londonappdev.com', 'test123')
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
